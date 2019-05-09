b12def91dbe83e0556b5814a848413667b4dd898
藤田竜伍 <fujitaryuugo@fujitaryuugonoMacBook-Air.local>
2019-05-09 19:05:12 +0900

    [Finish]3-5

--- sample_app/app/controllers/posts_controller.rb
+++ sample_app/app/controllers/posts_controller.rb
@@ -3,4 +3,23 @@ class PostsController < ApplicationController
       # 空のモデルをビューへ渡す
       @post = Post.new
   end
+  
+  # 以下を追加
+  def create
+  	  # ストロングパラメーターを使用
+  	   post = Post.new(post_params)
+      # DVへ保存する
+       post.save
+      #トップ画面へリダイレクト
+       redirect_to '/top'
+  end
+
+
+
+
+     private
+
+     def post_params
+     	 params.require(:post).permit(:title, :body)
+     end
 end
