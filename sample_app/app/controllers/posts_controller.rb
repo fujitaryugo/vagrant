29a8489f657c58b770442efb74e6c4ccec18f0f2
藤田竜伍 <fujitaryuugo@fujitaryuugonoMacBook-Air.local>
2019-05-12 13:37:09 +0900

    [Finish]3-8

--- sample_app/app/controllers/posts_controller.rb
+++ sample_app/app/controllers/posts_controller.rb
@@ -28,7 +28,7 @@ class PostsController < ApplicationController
   	  post = Post.new(post_params)
   	  post.save # DBへ保存する
   	  redirect_to post_path(post.id) # 詳細画面へリダイレクト
-  end 
+  end
 
   def edit 
       @post = Post.find(params[:id])
@@ -43,6 +43,6 @@ class PostsController < ApplicationController
      private
 
      def post_params
-     	 params.require(:post).permit(:title, :body)
+     	 params.require(:post).permit(:title, :body, :image)
      end
 end
--- sample_app/config/initializers/application_controller_renderer.rb
+++ sample_app/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '43ddce91150e688f1b32e249d16d54a94ae4704168063dbb8df3afd0f37228e5488ba7edfbbf19c6c6b589ee11e2c8f47fc637d338dc1fb5267e6ea7d7db243c'
