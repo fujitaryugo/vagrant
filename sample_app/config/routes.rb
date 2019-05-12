0212d155f18765e5c5679d316409ab979f418f8d
藤田竜伍 <fujitaryuugo@fujitaryuugonoMacBook-Air.local>
2019-05-12 11:55:15 +0900

    [Start]3-7

--- sample_app/config/routes.rb
+++ sample_app/config/routes.rb
@@ -9,4 +9,6 @@ Rails.application.routes.draw do
    get '/posts/:id' => 'posts#show', as: 'post'
    # .../posts/1 や .../posts/3 に該当する
 
+   get 'pots/:id/edit' => 'posts#edit', as 'edit_post'
+
 end
