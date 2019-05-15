d3a84035a6b0a96ce8ddf14e347a81087bcdbea5
藤田竜伍 <fujitaryuugo@fujitaryuugonoMacBook-Air.local>
2019-05-16 01:31:41 +0900

    [Finish]5-3

--- meshiterro/config/initializers/application_controller_renderer.rb
+++ meshiterro/config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '91add965045c751b710118b4b36d55d832c46416377660abc92ec5ce588928d6d42b1bf753327047db815b8e98d8e16eaea0117bda7ea4c3529efbd38678ee30'
